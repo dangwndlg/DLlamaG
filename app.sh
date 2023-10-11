3a7f1128b5e6113012ebbe24dc255f37c9967784
dangwndlg <gawd@directlinegroup.co.uk>
2023-10-11 11:16:11 +0100

    code dir

--- app.sh
+++ app.sh
@@ -4,5 +4,5 @@ if [[ -z "${DOMINO_NODE_IP}" ]]; then
     python src/main.py
 else
     # torchrun --n_proc_per_node 1 /mnt/src/main.py
-    python /mnt/src/main.py
+    python /mnt/code/src/main.py
 fi
