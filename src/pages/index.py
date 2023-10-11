eaaf50389cb1eb24a451ef9704391ede7210f8d0
dangwndlg <gawd@directlinegroup.co.uk>
2023-10-11 14:39:59 +0100

    User input

--- src/pages/index.py
+++ src/pages/index.py
@@ -1,9 +1,9 @@
-from dash import callback, html
-from dash.dependencies import Input, Output
+from dash import callback, dcc, html
+from dash.dependencies import Input, Output, State
 
 from utils import get_current_domino_user
 
-from typing import Any
+from typing import Any, List, Optional, Tuple, Union
 
 @callback(
     Output("current-user-header", "children"),
@@ -12,10 +12,42 @@ from typing import Any
 def current_user_header(_: Any) -> str:
     username: str = get_current_domino_user()
     return f"Welcome {username}"
+                
+@callback(
+    [
+        Output("chat-history-container", "children"),
+        Output("chat-input", "value")
+    ],
+    [
+        Input("chat-submit-button", "n_clicks"),
+        Input("chat-input", "n_submit")
+    ],
+    State("chat-input", "value"),
+    State("chat-history-container", "children")
+)
+def update_output_and_clear_input(
+    n_clicks: int,
+    n_submit: Optional[int], 
+    input_value: str, 
+    current_output: Optional[List[html.Div]]
+) -> Tuple[Union[Optional[List[html.Div]], str]]:
+    if n_clicks > 0 or bool(n_submit):
+        if input_value:
+            new_output = html.Div(input_value, style={"margin-bottom": "10px"})
+            return ([new_output] + current_output if current_output else [new_output], "")
+    return current_output, input_value
 
 def create_index_page() -> html.Div:
     return html.Div(children=[
         html.H1(id="current-user-header"),
+
+        dcc.Input(id="chat-input", type="text", placeholder="Enter text..."),
+        html.Button("Submit", id="chat-submit-button", n_clicks=0),
+        dcc.Loading(
+            id="loading-chat-history-container",
+            type="circle",
+            children=[html.Div(id="chat-history-container")],
+        ),
 
         html.Div(id="get-current-user-dummy", style={"display": "none"})
     ])
