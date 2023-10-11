e998174654bbc67d4e7c45b1175a36f2eececbb2
dangwndlg <gawd@directlinegroup.co.uk>
2023-10-11 15:48:30 +0100

    Loud llama install

--- app.sh
+++ app.sh
@@ -4,7 +4,7 @@ if [[ -z "${DOMINO_NODE_IP}" ]]; then
     pip install ./llama --quiet
     python src/main.py
 else
-    pip install /mnt/code/llama --user --quiet
+    pip install /mnt/code/llama --user
     # torchrun --n_proc_per_node 1 /mnt/src/main.py
     python /mnt/code/src/main.py
 fi
