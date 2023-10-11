5b912ccf38d20c1c3efd93c5c8920d3fceff57e8
dangwndlg <gawd@directlinegroup.co.uk>
2023-10-11 11:06:55 +0100

    undo test

--- app.sh
+++ app.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-printenv
-# if [[ -n "${!DOMINO_NODE_IP}" ]]; then
-#     # torchrun --n_proc_per_node 1 /mnt/src/main.py
-#     python /mnt/src/main.py
-# else
-#     python src/main.py
-# fi
+
+if [[ -n "${!DOMINO_NODE_IP}" ]]; then
+    # torchrun --n_proc_per_node 1 /mnt/src/main.py
+    python /mnt/src/main.py
+else
+    python src/main.py
+fi
