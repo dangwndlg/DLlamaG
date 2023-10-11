76bf964ea79eca07a4704c1b6f21e70ebf3d2da9
dangwndlg <gawd@directlinegroup.co.uk>
2023-10-11 16:10:45 +0100

    Allow build

--- src/config.py
+++ src/config.py
@@ -1,8 +1,8 @@
 import os
 
-BUILD_LLAMA: bool = False
 
 IS_DOMINO_ENV: bool = bool(os.getenv("DOMINO_NODE_IP"))
+BUILD_LLAMA: bool = IS_DOMINO_ENV
 
 LLAMA_CKPT_DIR: str = os.getenv("LLAMA_CKPT_DIR", "")
 LLAMA_TOKENIZER_PATH: str = os.getenv("LLAMA_TOKENIZER_PATH", "")
