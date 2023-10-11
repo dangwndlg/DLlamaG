c098cb71f8879a4f566678bcd5d58f586c43e8f5
dangwndlg <gawd@directlinegroup.co.uk>
2023-10-11 10:57:11 +0100

    TMP

--- app.sh
+++ app.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 if [[ -n "${!DOMINO_NODE_IP}" ]]; then
-    torchrun --n_proc_per_node 1 /mnt/src/main.py
+    # torchrun --n_proc_per_node 1 /mnt/src/main.py
+    python /mnt/src/main.py
 else
     python src/main.py
 fi
