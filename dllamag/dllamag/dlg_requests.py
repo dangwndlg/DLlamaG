37f8b0a77340a36d5eca123956131e89d1f5d196
dangwndlg <gawd@directlinegroup.co.uk>
2023-12-01 10:44:19 +0000

    dockerfile

--- dllamag/dllamag/dlg_requests.py
+++ dllamag/dllamag/dlg_requests.py
@@ -1,8 +1,11 @@
 from requests import Response, Session
 
 class DLGRequestManager:
-    def __init__(self) -> None:
+    def __init__(self, verify: bool = True) -> None:
         self.request_manager: Session = Session()
+
+        if not verify:
+            self.request_manager.verify = False
 
     def get(self, url: str, **kwargs) -> Response:
         return self.request_manager.get(url=url, **kwargs)
